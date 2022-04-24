8929830ae396479080705547e05a4c879fcbeee7
Brendan Dahl <dahl.brendan@gmail.com>
2022-04-24 12:41:17 -0400

    feat: add login error handling

--- src/blueair_api/http_blueair.py
+++ src/blueair_api/http_blueair.py
@@ -5,6 +5,7 @@ import base64
 
 from .util_http import request_with_logging
 from .const import API_KEY
+from .errors import AuthError
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,7 +25,7 @@ class HttpBlueair:
         self.auth_token = auth_token
 
         if client_session is None:
-            self.api_session = ClientSession(raise_for_status=True)
+            self.api_session = ClientSession(raise_for_status=False)
         else:
             self.api_session = client_session
 
@@ -88,7 +89,13 @@ class HttpBlueair:
                 url=url, headers=headers
             )
         )
-        return response.headers["X-AUTH-TOKEN"]
+        result = await response.text()
+        if response.status == 404:
+            raise AuthError("invalid username")
+        if result == "true":
+            return response.headers["X-AUTH-TOKEN"]
+        else:
+            raise AuthError("invalid password")
 
     async def get_devices(self) -> list[dict[str, any]]:
         """
