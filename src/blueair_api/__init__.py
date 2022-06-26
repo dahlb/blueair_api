0c1afe60ad726a932fe2fd7e987e7c2f5a428421
Brendan Dahl <dahl.brendan@gmail.com>
2022-06-26 13:47:02 -0400

    fix: add missing export

--- src/blueair_api/__init__.py
+++ src/blueair_api/__init__.py
@@ -3,3 +3,4 @@ from .http_blueair import HttpBlueair
 from .http_aws_blueair import HttpAwsBlueair
 from .util_bootstrap import get_devices, get_aws_devices
 from .device import Device
+from .device_aws import DeviceAws
