a8e3325e8b9d28b60c677a5aaf55b485f0590c54
Brendan Dahl <dahl.brendan@gmail.com>
2023-04-13 23:31:17 -0400

    chore: workflow improvements

---
+++ src/blueair_api/__init__.py
@@ -0,0 +1,6 @@
+from .errors import BaseError, RateError, AuthError, SessionError, LoginError
+from .http_blueair import HttpBlueair
+from .http_aws_blueair import HttpAwsBlueair
+from .util_bootstrap import get_devices, get_aws_devices
+from .device import Device
+from .device_aws import DeviceAws
