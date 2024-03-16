09c62c585737704483ec642586aa88ee374af62d
Jonathan Robichaud <jonathanrobichaud@icloud.com>
2024-03-16 15:01:56 -0300

    Add support for nightmode

--- src/blueair_api/device_aws.py
+++ src/blueair_api/device_aws.py
@@ -98,6 +98,11 @@ class DeviceAws(CallbacksMixin):
         self.child_lock = child_lock
         await self.api.set_device_info(self.uuid, "childlock", "vb", child_lock)
         self.publish_updates()
+        
+    async def set_night_mode(self, night_mode: bool):
+        self.night_mode = night_mode
+        await self.api.set_device_info(self.uuid, "nightmode", "vb", night_mode)
+        self.publish_updates()
 
     def __repr__(self):
         return {
