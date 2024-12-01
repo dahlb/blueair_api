7d315930b979863d48b4ed1f157ddf7c5bad93e2
Yu Feng <rainwoodman@gmail.com>
2024-12-01 15:23:59 -0800

    Correct model number.
    I took a final glance at the packaging before putting them away. It says H35i,
    not i35 on it.

--- src/blueair_api/model_enum.py
+++ src/blueair_api/model_enum.py
@@ -3,6 +3,6 @@ from enum import StrEnum
 
 class ModelEnum(StrEnum):
     UNKNOWN = "Unknown"
-    HUMIDIFIER_I35 = "Blueair Humidifier i35"
+    HUMIDIFIER_H35I = "Blueair Humidifier H35i"
     PROTECT_7470I = "Blueair Protect 7470i"
     MAX_311I = "311i Max"
