9cf7ec9e96a05170cb1fc4e7c18b08e3d140541d
Brendan Dahl <dahl.brendan@gmail.com>
2024-12-05 08:12:52 -0500

    feat: add supported features to the model enum

--- src/blueair_api/__init__.py
+++ src/blueair_api/__init__.py
@@ -4,4 +4,4 @@ from .http_aws_blueair import HttpAwsBlueair
 from .util_bootstrap import get_devices, get_aws_devices
 from .device import Device
 from .device_aws import DeviceAws
-from .model_enum import ModelEnum
+from .model_enum import ModelEnum, FeatureEnum
