6041f93c424d339a8c516033b7e89f582e89a460
Yu Feng <rainwoodman@gmail.com>
2024-12-11 01:46:09 -0800

    use the new type statement.

--- src/blueair_api/device_aws.py
+++ src/blueair_api/device_aws.py
@@ -8,6 +8,7 @@ from . import ir_aws as ir
 
 _LOGGER = logging.getLogger(__name__)
 
+type AttributeType[T] = T | None | type[NotImplemented]
 
 @dataclasses.dataclass(slots=True)
 class DeviceAws(CallbacksMixin):
@@ -27,39 +28,39 @@ class DeviceAws(CallbacksMixin):
         return device_aws
 
     api: HttpAwsBlueair
-    uuid: str  | None | type[NotImplemented] = None
-    name: str  | None | type[NotImplemented] = None
-    name_api: str  | None | type[NotImplemented] = None
-    mac: str  | None | type[NotImplemented] = None
-    type_name: str  | None | type[NotImplemented] = None
+    uuid : AttributeType[str] = None
+    name : AttributeType[str] = None
+    name_api : AttributeType[str] = None
+    mac : AttributeType[str] = None
+    type_name : AttributeType[str] = None
 
-    sku: str  | None | type[NotImplemented] = None
-    firmware: str  | None | type[NotImplemented] = None
-    mcu_firmware: str  | None | type[NotImplemented] = None
-    serial_number: str  | None | type[NotImplemented] = None
+    sku : AttributeType[str] = None
+    firmware : AttributeType[str] = None
+    mcu_firmware : AttributeType[str] = None
+    serial_number : AttributeType[str] = None
 
-    brightness: int  | None | type[NotImplemented] = None
-    child_lock: bool  | None | type[NotImplemented] = None
-    fan_speed: int  | None | type[NotImplemented] = None
-    fan_auto_mode: bool  | None | type[NotImplemented] = None
-    standby: bool  | None | type[NotImplemented] = None
-    night_mode: bool  | None | type[NotImplemented] = None
-    germ_shield: bool  | None | type[NotImplemented] = None
+    brightness : AttributeType[int] = None
+    child_lock : AttributeType[bool] = None
+    fan_speed : AttributeType[int] = None
+    fan_auto_mode : AttributeType[bool] = None
+    standby : AttributeType[bool] = None
+    night_mode : AttributeType[bool] = None
+    germ_shield : AttributeType[bool] = None
 
-    pm1: int  | None | type[NotImplemented] = None
-    pm2_5: int  | None | type[NotImplemented] = None
-    pm10: int  | None | type[NotImplemented] = None
-    tVOC: int  | None | type[NotImplemented] = None
-    temperature: int  | None | type[NotImplemented] = None
-    humidity: int  | None | type[NotImplemented] = None
-    filter_usage: int  | None | type[NotImplemented] = None  # percentage
-    wifi_working: bool  | None | type[NotImplemented] = None
+    pm1 : AttributeType[int] = None
+    pm2_5 : AttributeType[int] = None
+    pm10 : AttributeType[int] = None
+    tVOC : AttributeType[int] = None
+    temperature : AttributeType[int] = None
+    humidity : AttributeType[int] = None
+    filter_usage : AttributeType[int] = None  # percentage
+    wifi_working : AttributeType[bool] = None
 
     # i35
-    wick_usage: int  | None | type[NotImplemented] = None  # percentage
-    wick_dry_mode: bool  | None | type[NotImplemented] = None
-    water_shortage: bool  | None | type[NotImplemented] = None
-    auto_regulated_humidity: int  | None | type[NotImplemented] = None
+    wick_usage : AttributeType[int] = None  # percentage
+    wick_dry_mode : AttributeType[bool] = None
+    water_shortage : AttributeType[bool] = None
+    auto_regulated_humidity : AttributeType[int] = None
 
     async def refresh(self):
         _LOGGER.debug(f"refreshing blueair device aws: {self}")
